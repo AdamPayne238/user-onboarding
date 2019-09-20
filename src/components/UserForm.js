cd026c4428ab5ec7e273d0efe2725e21e92b7875
Adam Payne <ajpayne238@gmail.com>
2019-09-19 20:28:05 -0400

    styled some

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -10,11 +10,39 @@ import styled from "styled-components";
 
 
 const FormHeader = styled.h1`
-  text-align: center;
-  border: 4px solid black;
-  color: black;
+width: 35%;
+margin: auto;
+text-align: center;
+border: 4px solid black;
+color: black;
 `;
 
+const FormFields = styled.div`
+height: 500px;
+border: 4px solid black;
+width: 35%;
+display: flex;
+flex-direction: column;
+margin: auto;
+input{
+    font-size: 2rem;
+    margin: 20px
+    height: 3rem;
+    border: 2px solid black;
+};
+`;
+
+const FormButton = styled.button`
+width: 50%;
+height: 3rem;
+border: 2px solid black;
+border-radius: 20px;
+font-size: 2rem;
+margin: auto;
+max-height: 100%;
+max-width: 100%;
+
+`;
 
 const UserForm = ({ values, errors, touched, status }) => {
 
@@ -30,18 +58,24 @@ const UserForm = ({ values, errors, touched, status }) => {
             <FormHeader>I am form!</FormHeader>
 
             <Form>
-
+                <FormFields>
                 <Field type="text" name="Name" placeholder="Name" />
-
+                {touched.Name && errors.Name && (
+                <p className="error">{errors.Name}</p>)}
+            
                 <Field type="text" name="Email" placeholder="Email" />
+                {touched.Email && errors.Email && (
+                <p className="error">{errors.Email}</p>)}
 
                 <Field type="text" name="Password" placeholder="Password" />
+                {touched.Password && errors.Password && (
+                <p className="error">{errors.Password}</p>)}
 
                 <span>Terms of Service</span>
                 <Field type="checkbox" name="Terms" checked={values.Terms} />
-                
-                <button>Submit</button>
-
+                       
+                <FormButton>Submit</FormButton>
+                </FormFields>
             </Form>
             
             {user.map(users => (
